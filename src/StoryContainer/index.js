94be19247985c902958a9d2c30b69cec2e678f66
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-02-18 16:13:10 -0600

    unable to access stories because we are not logged in

--- src/StoryContainer/index.js
+++ src/StoryContainer/index.js
@@ -3,10 +3,40 @@ import React from 'react'
 class StoryContainer extends React.Component {
 	constructor(props) {
 		super(props)
+
+		this.state = {
+			stories: []
+		}
+	}
+
+	//component lifecycle method
+	//instantiate the network request
+	componentDidMount() {
+
+		this.getStories()
+	}
+
+	getStories = async () => {
+		try {
+			//response from the request
+			//fetch the data
+			const storiesResponse = await fetch(process.env.REACT_APP_API_URL + "/api/v1/stories/")
+
+			//json of response
+			const storiesJson = await storiesResponse.json()
+			console.log('data from getStories() in StoryContainer')
+			console.log(storiesJson)
+
+
+		} catch(err) {
+			console.log(err);
+		}
 	}
 
 
 	render() {
+		console.log(this.state)
+
 		return(
 			<React.Fragment>
 			<h4>Story Container</h4>
